05de4eabbe1cda91011f8211afb83a79d73c6449
Nxirda <3188097+Nxirda@users.noreply.github.com>
2026-02-12 19:00:28 +0100

    Lgtm

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,7 +4,7 @@
 # duplicate find_package calls and expose all dependencies in a single file.
 
 find_package(nlohmann_json REQUIRED)
-find_package(Boost REQUIRED COMPONENTS system)
+find_package(Boost REQUIRED COMPONENTS process)
 find_package(sciplot REQUIRED)
 find_package(LLVM REQUIRED CONFIG)
 find_package(fmt REQUIRED)
